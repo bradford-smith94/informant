e5e6052e7b2fb88bdd93bcef9892c0b8db003129
Bradford Smith <bradford.smith94@gmail.com>
2021-02-06 14:46:15 -0500

    Make setup.py get version number from main file
    This makes use of regex to find the version string in the main module
    file and use that for packaging.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import re
 from setuptools import setup
+
+# read version string from main file
+_version = '0.0.0'
+with open('./informant/informant.py') as version_file:
+    file_dat = version_file.read()
+    _version = re.findall(r"^__version__ = '(\d+\.\d+\.\d+)'", file_dat, re.MULTILINE)[0]
 
 setup(
         name='informant',
-        version='0.3.0',
+        version=_version,
         author='Bradford Smith',
         author_email='aur@bradfords.me',
         packages=['informant'],
